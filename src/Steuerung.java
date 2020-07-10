23330be60bde4299ea0f8b06be3f3103653ce1d2
dbauer <denis.bauer@kardex.com>
2020-07-10 14:49:58 +0200

    anfang gui

--- src/Steuerung.java
+++ src/Steuerung.java
@@ -1,13 +1,16 @@
 import regal.Regalfach;
+import gui.GUI;
 public class Steuerung {
     Regalfach dasRegal[][] = new Regalfach[3][3];
-
+    GUI dieGui;
     public Steuerung(){
         init();
     }
     private void init(){
 		for(int n=0;n<3;n++)
 			for(int m=0;m<3;m++)
-				dasRegal[n][m] = new Regalfach(n, m);
+                dasRegal[n][m] = new Regalfach(n, m);
+                
+        dieGui = new GUI();
 	}
 }
