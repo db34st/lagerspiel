4fb4c0e41d8fbc74f293ba9bca6fa47c7aeaced5
dbauer <denis.bauer@kardex.com>
2020-07-13 15:06:05 +0200

    new class Auftrag

--- src/auftraege/Auftrag.java
+++ src/auftraege/Auftrag.java
@@ -1,4 +1,26 @@
 package auftraege;
+import produkte.*;
 public class Auftrag {
+	static int idCounter;
+    private int aId, aBelohnung;
+    private boolean aAuftragsArt; //true=einlagern, false=auslagern
+    private Produkt aProdukt;
     
+    public Auftrag(String pProdukt, String pAttr1, String pAttr2, int pBelohnung) throws Exception{
+    	aId = idCounter++;
+    	aBelohnung = pBelohnung;
+    	switch(pProdukt) {
+    	case "Papier": 
+    		aProdukt = new Papier(pAttr1, pAttr2);
+    		break;
+    	case "Holz": 
+    		aProdukt = new Holz(pAttr1, pAttr2);
+    		break;
+    	case "Stein": 
+    		aProdukt = new Stein(pAttr1, pAttr2);
+    		break;
+    		default:
+    			throw new Exception("eingelesenes Produkt nicht vorhanden!");
+    	}
+    }
 }
