39bec8938b171c4fb22cde38bf5898a77d0b8b2c
dbauer <denis.bauer@kardex.com>
2020-07-14 14:20:16 +0200

    add bilanz + Bilanzeintrag

--- src/bilanz/Bilanz.java
+++ src/bilanz/Bilanz.java
@@ -1,4 +1,33 @@
 package bilanz;
+
 public class Bilanz {
-    
+    private Bilanzeintrag anf, pos;
+    public Bilanz(Bilanzeintrag pEintrag) {
+    	pos = anf = pEintrag;
+    	anf.next = null;
+    }
+    public boolean empty() {
+		return anf.next == null;
+	}
+	public boolean endpos() {
+		return pos.next == null;
+	}
+	public void reset() {
+		pos = anf;
+	}
+    public void advance() {
+    	if(endpos()) pos = anf;
+    	else pos = pos.next;
+    }
+    public Bilanzeintrag elem() {
+    	return pos;
+    }
+    public void neuerEintrag(Bilanzeintrag pEintrag) {
+    	pEintrag.next = pos.next;
+    	pos.next = pEintrag;
+    	advance();
+    }
+    public void entferneAuftrag() {
+    	pos.next = pos.next.next;
+    }
 }
--- src/bilanz/Bilanzeintrag.java
+++ src/bilanz/Bilanzeintrag.java
@@ -1,4 +1,12 @@
 package bilanz;
+import auftraege.Auftrag;
 public class Bilanzeintrag {
-
+	private Auftrag aAuftrag;
+	private boolean aAusgefuehrt;
+	public Bilanzeintrag next;
+	
+	public Bilanzeintrag(Auftrag aAuftrag, boolean aAusgefuehrt) {
+		this.aAuftrag = aAuftrag;
+		this.aAusgefuehrt = aAusgefuehrt;
+	}
 }
